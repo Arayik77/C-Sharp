ac33b5e5acb27c647d99d2b3eecab31a2943eead
Arayik77 <arayik_grigoryan_97@mail.ru>
2023-03-24 22:16:42 +0400

    class & struct , CONSTRUCTOR

---
+++ C-sharp-HomeRead/Constructor/Person.cs
@@ -0,0 +1,74 @@
+﻿using System.Xml.Linq;
+
+namespace Constructor
+{
+    internal class Person
+    {
+        public string SecondName { get; set; }
+        public string FirstName { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + SecondName;
+            }
+        }
+
+        public string ShortName
+        {
+            get
+            {
+                return $"{SecondName} {FirstName.Substring(0, 1)}.";
+            }
+        }
+
+        // constructor start
+
+        public Person(string firstName, string secondName)
+        {
+            firstName = FirstName;
+            secondName = SecondName;
+        }
+
+        // constructor end
+
+        ////public string Name;
+
+        //private string _name;
+
+        //public string Name
+        //{
+        //    get
+        //    {
+        //        return _name;
+        //    }
+
+        //    set
+        //    {
+        //        if (string.IsNullOrWhiteSpace(value))
+        //        {
+        //            throw new ArgumentNullException("anuny chi karox line   l datark");
+        //        }
+
+        //        _name = value;
+        //    }
+        //} // nuyn banna inch nerqevi comment um exacy
+
+
+
+        ////public string GetName()
+        ////{
+        ////    return _name;
+        ////}
+
+        ////public void SetName(string name)
+        ////{
+        ////    if(string.IsNullOrWhiteSpace(name))
+        ////    {
+        ////        throw new ArgumentNullException("anuny chi karox linel datark");
+        ////    }
+        ////        _name = name;
+        ////}
+    }
+}
---
+++ C-sharp-HomeRead/Constructor/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // constructor start
+
+            Person person = new Person("Test", "Testyan");
+            Console.WriteLine(person.FullName);
+            Console.WriteLine(person.ShortName);
+            Console.ReadLine();
+
+            // constructor end
+
+
+            //Person person = new Person();
+            //person.FirstName = "Test";
+            //person.SecondName = "Testyan";
+            //Console.WriteLine(person.FullName);
+            //Console.WriteLine(person.ShortName);
+            //Console.ReadLine();
+
+            //person.SetName("Test");
+            //Console.WriteLine(person.GetName());
+            //Console.ReadLine(); // Person class i meji comment i hamar er sa
+
+            // person.Name = "Test"; // public linelu jamanak
+        }
+    }
+}
