cb340d3140946fec179f18bc446e3950fa5ad5c6
Arayik77 <arayik_grigoryan_97@mail.ru>
2023-03-25 12:56:11 +0400

    Film Boom

---
+++ C-sharp-HomeWork/FilmBoom/Films.cs
@@ -0,0 +1,8 @@
+﻿namespace FilmBoom
+{
+    internal class Films
+    {
+        string[] FilmsCategories = { "Category1", "Category2", "Category3" };
+        string[] Films = { "Film1", "Film2", "Film3" };
+    }
+}
