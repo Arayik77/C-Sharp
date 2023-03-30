5d2315b6fecc132eb2f6c5a1b64a4dc6f117a3e4
Arayik77 <arayik_grigoryan_97@mail.ru>
2023-03-30 21:10:48 +0400

    Lesson11

---
+++ C-sharp-video-youtube/Lesson11/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Lesson11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            // a++; // 2
+            a--;
+            Console.WriteLine(a);
+        }
+    }
+}
